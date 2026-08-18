4f1bab8b63d1e15bca6dbd1484f9018d7d29081f
levoweal <69198520+levoweal@users.noreply.github.com>
2026-08-18 09:16:17 +0400

    initial api fetch setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import "./styles.css";
+
+const content = document.querySelector('.content');
+
+(() => {
+    const label = document.createElement('label');
+    label.textContent = 'city';
+    label.htmlFor = 'search';
+
+    const input = document.createElement('input');
+    input.id = 'search';
+    input.type = 'text';
+
+    const button = document.createElement('button');
+    button.className = 'confirm';
+    button.textContent = 'search';
+
+    button.addEventListener('click', () => {
+        if (!input.value) {
+            input.setCustomValidity('type something first');
+            input.reportValidity();
+            return;
+        };
+        getWeatherData(input.value);
+    })
+
+    label.appendChild(input);
+    content.appendChild(label);
+    content.appendChild(button);
+})()
+
+async function getWeatherData(city) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=current&key=MYZTXBP9J8NAAXBJL3YLMKH94&contentType=json`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
