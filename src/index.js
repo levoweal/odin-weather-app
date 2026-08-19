391edca8b616ac49001779824a2c1ed7c2bd7401
levoweal <69198520+levoweal@users.noreply.github.com>
2026-08-19 11:06:19 +0400

    basic display for main

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import "./styles.css";
 const content = document.querySelector('.content');
 
 (() => {
+    //searchbar stuff
+    const searchbar = document.querySelector('.searchbar');
+
     const label = document.createElement('label');
     label.textContent = 'city';
     label.htmlFor = 'search';
@@ -15,23 +18,48 @@ const content = document.querySelector('.content');
     button.className = 'confirm';
     button.textContent = 'search';
 
-    button.addEventListener('click', () => {
+    button.addEventListener('click', async () => {
         if (!input.value) {
             input.setCustomValidity('type something first');
             input.reportValidity();
             return;
         };
-        getWeatherData(input.value);
+
+        const data = await getWeatherData(input.value);
+
+        updateMain(data);
     })
 
     label.appendChild(input);
-    content.appendChild(label);
-    content.appendChild(button);
+    searchbar.appendChild(label);
+    searchbar.appendChild(button);
 })()
 
 async function getWeatherData(city) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=current&key=MYZTXBP9J8NAAXBJL3YLMKH94&contentType=json`);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=MYZTXBP9J8NAAXBJL3YLMKH94&contentType=json`);
     const data = await response.json();
     console.log(data);
+    data.isActive = false;
     return data;
+}
+
+function updateMain(data) {
+    const main = document.querySelector('.main');
+    main.textContent = '';
+
+    const current = document.createElement('div');
+    current.textContent = data.currentConditions.temp;
+    current.className = 'current-temperature';
+
+    const conditions = document.createElement('div');
+    conditions.textContent = data.currentConditions.conditions;
+    conditions.className = 'current-conditions';
+
+    const feels = document.createElement('div');
+    feels.textContent = `Feels like ${data.currentConditions.feelslike}`;
+    feels.className = 'current-feels-like';
+
+    main.appendChild(current);
+    main.appendChild(conditions);
+    main.appendChild(feels);
 }
