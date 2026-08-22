9aea2d3db26dc06a49d7ac78e6057b7cca9ed9ca
levoweal <69198520+levoweal@users.noreply.github.com>
2026-08-22 11:34:39 +0400

    dynamic display for hours and days

--- src/index.js
+++ src/index.js
@@ -35,8 +35,10 @@ let data;
 
         data = await getWeatherData(input.value);
 
-        updateMain(data);
-        updateHours(data);
+        const currentHour = data.days[0].hours.find(hour => hour.datetime.slice(0, 2) === data.currentConditions.datetime.slice(0, 2));
+
+        updateMain(currentHour);
+        updateHours(data.days[0]);
         updateDays(data);
     })
 
@@ -45,27 +47,31 @@ let data;
     searchbar.appendChild(button);
 })()
 
-function updateMain(data) {
+function updateMain(hour) {
     const container = document.querySelector('.main');
     container.textContent = '';
 
-    container.appendChild(elF('div', data.currentConditions.datetime, 'current-time'));
-    container.appendChild(elF('div', data.currentConditions.temp, 'current-temperature'));
-    container.appendChild(elF('div', data.currentConditions.conditions, 'current-conditions'));
-    container.appendChild(elF('div', `Feels like ${data.currentConditions.feelslike}`, 'current-feels-like'));
+    container.appendChild(elF('div', hour.datetime, 'current-time'));
+    container.appendChild(elF('div', hour.temp, 'current-temperature'));
+    container.appendChild(elF('div', hour.conditions, 'current-conditions'));
+    container.appendChild(elF('div', `Feels like ${hour.feelslike}`, 'current-feels-like'));
 }
 
-function updateHours(data) {
+function updateHours(day) {
     const container = document.querySelector('.hours');
     container.textContent = '';
 
     for (const i of [0, 3, 7, 10, 14, 17, 21]) {
         const con = elF('div', '', 'current-hour-container');
-        const hour = data.days[0].hours[i];
+        const hour = day.hours[i];
 
         con.appendChild(elF('div', hour.datetime, 'current-hour-time'));
         con.appendChild(elF('div', hour.temp, 'current-hour-temperature'));
         con.appendChild(elF('div', hour.conditions, 'current-hour-conditions'));
+
+        con.addEventListener('click', () => {
+            updateMain(hour);
+        })
 
         container.appendChild(con);
     }
@@ -75,12 +81,17 @@ function updateDays(data) {
     const container = document.querySelector('.days');
     container.textContent = '';
 
-    for (const day of data.days.slice(1, 8)) {
+    for (const day of data.days.slice(0, 7)) {
         const con = elF('div', '', 'day-container');
 
         con.appendChild(elF('div', format(new Date(day.datetime), 'EEEE'), 'day-date'));
         con.appendChild(elF('div', day.conditions, 'day-conditions'));
         con.appendChild(elF('div', day.temp, 'day-average-temperature'));
+
+        con.addEventListener('click', () => {
+            updateMain(day.hours[12]);
+            updateHours(day);
+        })
 
         container.appendChild(con);
     }
